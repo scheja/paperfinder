24949969eec16ac2ecbd94326ddd99b8e2cf28b2
Jan Scheurenbrand <mail@janscheurenbrand.de>
2014-12-03 07:49:28 +0100

    Refactor Response entity parsing

--- src/main/java/edu/kit/ksri/paperfinder/scholar/http/Response.java
+++ src/main/java/edu/kit/ksri/paperfinder/scholar/http/Response.java
@@ -3,6 +3,7 @@ package edu.kit.ksri.paperfinder.scholar.http;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,14 +31,9 @@ public class Response {
                 this.statusCode = response.getStatusLine().getStatusCode();
                 logger.info(String.format("Executed Response. Status Code %s", this.statusCode));
                 BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-                StringBuilder result = new StringBuilder();
-                String line = "";
-                while ((line = rd.readLine()) != null) {
-                    result.append(line);
-                }
-                this.responseBody = result.toString();
-                //logger.info("Response Content:");
-                //logger.info(this.responseBody);
+                this.responseBody = EntityUtils.toString(response.getEntity());
+                logger.info("Response Content:");
+                logger.info(this.responseBody);
             } finally {
                 response.close();
             }
